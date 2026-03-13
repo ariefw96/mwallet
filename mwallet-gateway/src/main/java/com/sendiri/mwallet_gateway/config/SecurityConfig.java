cabadbd484fc9d61ece0f25a12a5fcdf54076397
ariefw <ariefwidiyatmoko38@gmail.com>
2026-03-13 15:10:18 +0700

    fix security config (part 1)

--- mwallet-gateway/src/main/java/com/sendiri/mwallet_gateway/config/SecurityConfig.java
+++ mwallet-gateway/src/main/java/com/sendiri/mwallet_gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sendiri.mwallet_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,8 +24,9 @@ public class SecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(redisAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
